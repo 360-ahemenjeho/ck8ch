6c4306e71713624126aa0b071d3a442811bb181d
360-ahemenjeho <159141014+360-ahemenjeho@users.noreply.github.com>
2025-04-28 00:43:22 +0100

    chore(insights & analytics)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { mount } from 'svelte'
+import { inject } from '@vercel/analytics'
+import { injectSpeedInsights } from '@vercel/speed-insights'
 import './app.css'
 import App from './App.svelte'
 
+inject()
+injectSpeedInsights()
+
 const app = mount(App, {
-  target: document.getElementById('app')!,
+  target: document.getElementById('app')!
 })
 
 export default app
